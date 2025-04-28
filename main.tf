e6801477af36a01670722008f0792a359c3a96dc
shreyaa224 <148742293+shreyaa224@users.noreply.github.com>
2025-04-28 10:13:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ provider "azurerm" {
   subscription_id = "d39add2e-a062-4ac2-b217-b69b4fb14a19"
 }
 resource "azurerm_resource_group" "example" {
-  for_each = toset(["rg2204_01", "rg2204_02", "rg2204_03"])
+  for_each = toset(["rg220401", "rg220402", "rg220403"])
   name     = "resourceGroup-${each.value}"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "example" {
   for_each = {
-    rg2204_01 = "eastus2"
-    rg2204_02 = "westus"
-    rg2204_03 = "centralus"
+    rg220401 = "eastus2"
+    rg220402 = "westus"
+    rg220403 = "centralus"
   }
   name                     = "storage${each.key}"
   resource_group_name      = azurerm_resource_group.example[each.key].name
